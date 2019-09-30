fe08c881468847f1bb817f18939b4d695c2658d2
jhank09 <jcroldan3@misena.edu.co>
2019-09-30 10:08:44 -0500

    cambiar titulo

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Visual Solutions</title>
+  <title>Visual Solutionssss</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
