cc215acd8734d7c87dfcea58773979d90d0ab38f
federicoangel <51235308+federicoangel@users.noreply.github.com>
2019-10-01 09:35:41 -0500

    Update agregar.php
    lol

--- application/view/clientes/agregar.php
+++ application/view/clientes/agregar.php
@@ -12,7 +12,7 @@
             <input type="number" name="telefono" value="" required />
             <input type="submit" class="btn btn-success" name="btnguardar" value="Guardar" />
             <br>
-            <a type="button" class="btn btn-outline-primary float-sm-right" name="regresar" role="button" href="<?php echo URL; ?>clientes" />Regresar a clientes</a>
+            <a type="button" class="btn btn-outline-primary float-sm-right" name="regresar" role="button" href="<?php echo URL; ?>clientes" />Regresar</a>
         </form>
 
     </div>
