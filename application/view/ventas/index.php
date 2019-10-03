903ac68d0cf869d43eb0fc0b3e7f8ef9562b509b
JoTaRt <jjramirez480@misena.edu.co>
2019-10-03 06:49:49 -0500

    venta con names

--- application/view/ventas/index.php
+++ application/view/ventas/index.php
@@ -22,8 +22,8 @@
           <tr>
               <td><?php if (isset($venta->valor_venta)) echo htmlspecialchars($venta->valor_venta, ENT_QUOTES, 'UTF-8'); ?></td>
               <td><?php if (isset($venta->fecha_venta)) echo htmlspecialchars($venta->fecha_venta, ENT_QUOTES, 'UTF-8'); ?></td>
-              <td><?php if (isset($venta->id_cliente)) echo htmlspecialchars($venta->id_cliente, ENT_QUOTES, 'UTF-8'); ?></td>
-              <td><?php if (isset($venta->id_usuario)) echo htmlspecialchars($venta->id_usuario, ENT_QUOTES, 'UTF-8'); ?></td>
+              <td><?php if (isset($venta->cliente)) echo htmlspecialchars($venta->cliente, ENT_QUOTES, 'UTF-8'); ?></td>
+              <td><?php if (isset($venta->usuario)) echo htmlspecialchars($venta->usuario, ENT_QUOTES, 'UTF-8'); ?></td>
               <td><a class="btn btn-success" href="<?php echo URL . 'Ventas/editar/' . htmlspecialchars($venta->id_venta, ENT_QUOTES, 'UTF-8'); ?>"><i class="mdi mdi-lead-pencil"></i></a></td>
               <td><a class="btn btn-danger" href="<?php echo URL . 'Ventas/eliminar/' . htmlspecialchars($venta->id_venta, ENT_QUOTES, 'UTF-8'); ?>"><i class="mdi mdi-delete"></i></a></td>
           </tr>
