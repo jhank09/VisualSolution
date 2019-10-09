c037a67e33d46383534aa75702b115a21d0d8665
fenix8908 <marialuci1019@gmail.com>
2019-10-09 11:32:03 -0500

    Cambio footer
    Se agrego nuevos archivos al login

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,15 +1,14 @@
 </div>
 </div>
-</body>
+
 
 <script src="<?php echo URL; ?>js/validacion_producto.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/ jquery-validation@1.19.1 /dist/jquery.validate.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/ jquery-validation@1.19.1 /dist/additional-methods.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.4/build/alertify.min.js"></script>
 <script src="<?php echo URL; ?>assets/libs/jquery/dist/jquery.min.js"></script>
 <script src="<?php echo URL; ?>assets/libs/popper.js/dist/umd/popper.min.js"></script>
 <script src="<?php echo URL; ?>assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -26,11 +25,28 @@
 <script src="<?php echo URL; ?>assets/libs/flot/jquery.flot.crosshair.js"></script>
 <script src="<?php echo URL; ?>assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
 <script src="<?php echo URL; ?>dist/js/pages/chart/chart-page-init.js"></script>
-<script src="<?php echo URL; ?>js/ventas.js"></script>
-
-
+<script src="<?php echo URL; ?>assets/datatables/datatables.min.js"></script>
+<script src="<?php echo URL; ?>assets/datatables/datatables.min.js"></script>
+<script src="<?php echo URL; ?>js/main_1.js"></script>
+<script src="<?php echo URL; ?>js/password_strength.js"></script>
+<script src="<?php echo URL; ?>js/jquery-strength.js"></script>
+<script src="<?php echo URL; ?>js/seguridad_pass.js"></script>
 <script>
-var url = "<?php echo URL; ?>";
+    function myFunction() {
+        var x = document.getElementById("myInput");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
 </script>
+<script>
+    var uri = "<?php echo URL; ?>";
+</script>
+<script src="<?php echo URL; ?>js/misJS.js"></script>
+<script src="<?php echo URL; ?>js/validar_prod.js"></script>
+
+</body>
 
 </html>
