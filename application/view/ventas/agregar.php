5e92e3ae0f129c70d51e1d8eadb8f415a56f18b2
JoTaRt <jjramirez480@misena.edu.co>
2019-10-09 09:06:43 -0500

    vista ventas2

--- application/view/ventas/agregar.php
+++ application/view/ventas/agregar.php
@@ -31,60 +31,61 @@
       </div>
       <div class="row">
         <div class="col-md-4">
-          <label>Producto</label>
-          <select class="form-control" name="id_producto" id="id_producto" required>
-            <option>Seleccione</option>
-            <?php foreach ($producto as $p): ?>
-              <option value="<?= $p->id_producto ?>"><?= $p->nombre_producto ?></option>
-            <?php endforeach; ?>
-          </select>
-        </div>
-        <div class="col-md-8">
-          <table class="table" id="tb_productos">
-            <thead>
-              <tr>
-                <th>producto</th>
-                <th>cantidad</th>
-                <th>opción</th>
-              </tr>
-            </thead>
-            <tbody>
+          <div class="form-group">
+            <label>Producto</label>
+            <select class="form-control" name="id_producto" id="id_producto" required>
+              <option>Seleccione</option>
+              <?php foreach ($producto as $p): ?>
+                <option value="<?= $p->id_producto ?>"><?= $p->nombre_producto ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <div class="form-group">
+            <label>cantidad</label>
+            <input class="form-control" id="cantidad" type="number" name="cantidad" value="" required min="1"/>
+          </div>
+            <button class="btn btn-primary float-right" type="button" onclick="agregar_producto()">Agregar producto</button>
+          </div>
+          <div class="col-md-8">
+            <table class="table" id="tb_productos">
+              <thead>
+                <tr>
+                  <th>producto</th>
+                  <th>cantidad</th>
+                  <th>opción</th>
+                </tr>
+              </thead>
+              <tbody>
 
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
         </div>
+
+
+
+          </div>
+
+        <br>
+        <input class="btn btn-success" onclick="validartp();" type="submit" name="btnguardar" value="Guardar" />
+      </form>
       </div>
-      <div class="row">
-        <div class="col-md-4">
-          <label>cantidad</label>
-          <input class="form-control" id="cantidad" type="number" name="cantidad" value="" required min="1"/>
-          <br>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4">
-          <button class="btn btn-primary float-right" type="button" onclick="agregar_producto()">Agregar producto</button>
-        </div>
-      </div>
-      <br>
-      <input class="btn btn-success" onclick="validartp();" type="submit" name="btnguardar" value="Guardar" />
-    </form>
-  </div>
+    </div>
 
 
 
 
 
-  <script>
-  function validarSiNumero(valor){
+    <script>
+    function validarSiNumero(valor){
 
-    if (!/^([0-9])*$/.test(valor)){
-      Swal.fire({
-        type: 'error',
-        title: 'Oops...',
-        text: 'Solo numeros positivos',
-      })
-      return false;
+      if (!/^([0-9])*$/.test(valor)){
+        Swal.fire({
+          type: 'error',
+          title: 'Oops...',
+          text: 'Solo numeros positivos',
+        })
+        return false;
+      }
     }
-  }
-</script>
+  </script>
