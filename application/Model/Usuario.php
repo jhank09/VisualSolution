a04651abb9a3e4d56b38aa677d4bd6c37baaaf27
fenix8908 <marialuci1019@gmail.com>
2019-10-09 10:32:20 -0500

    terminar modelo Usuarios
    se subieron cambios al modelo de usuarios

--- application/Model/Usuario.php
+++ application/Model/Usuario.php
@@ -12,5 +12,63 @@ class Usuario extends Model {
         $stm->execute();
         return $stm->fetchAll();
     }
+     public function detalle($id) {
+        $sql = "SELECT * FROM usuarios  WHERE id_usuario = :id LIMIT 1";
+        $consulta = $this->db->prepare($sql);
+        $parametros = array(':id' => $id);
+        $consulta->execute($parametros);
+        return $consulta->rowcount() ? $consulta->fetch() : false;
+    }
+
+    public function cambiar_estado() {
+        $sql = "UPDATE usuarios SET estado = ? WHERE id_usuario = ?";
+        $stm = $this->db->prepare($sql);
+        $stm->bindParam(1, $this->estado);
+        $stm->bindParam(2, $this->id_usuario);
+
+        return $stm->execute();
+    }
+
+    private function ultimo() {
+        $sql = "SELECT MAX(id_usuario) as id FROM usuarios";
+        $stm = $this->db->prepare($sql);
+        $stm->execute();
+        return $stm->fetch();
+    }
+
+    public function agregarUsuario() {
+
+        $sql = "INSERT INTO usuarios  VALUES(null,?,?,?,?)";
+        $consulta = $this->db->prepare($sql);
+        $consulta->bindParam(1, $this->correo);
+        $consulta->bindParam(2, $this->contraseña);
+        $consulta->bindParam(3, $this->rol);
+        $consulta->bindParam(4, $this->estado);
+        if ($consulta->execute()) {
+            return $this->ultimo();
+        } else {
+            return false;
+        }
+    }
+
+    public function eliminarUser($id) {
+
+        $sql = "DELETE FROM usuarios WHERE id_usuario = :id";
+        $consulta = $this->db->prepare($sql);
+        $parametros = array(":id" => $id);
+        $consulta->execute($parametros);
+    }
+
+    public function editarUsuario() {
+        $sql = "UPDATE usuarios SET correo = ?,contraseña = ?,id_rol=?,estado =? WHERE id_usuario=?";
+        $consulta = $this->db->prepare($sql);
+        $consulta->bindParam(1, $this->correo);
+        $consulta->bindParam(2, $this->contraseña);
+        $consulta->bindParam(3, $this->rol);
+        $consulta->bindParam(4, $this->estado);
+        $consulta->bindParam(5, $this->id_usuario);
+        return $consulta->execute();
+    }
+
 
 }
